569ca0efc03e3e07bed2aab12f87f5370693ca30
Willy Stadnick <willy@e3c.com.br>
2014-02-20 18:18:51 -0300

    Continue...

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,15 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 from polls.models import Poll
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
